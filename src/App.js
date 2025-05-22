817058112de8405b499d6b48fe22ee72fa05b88a
diogo <diogo.lima.flooxai@gmail.com>
2025-05-22 10:40:07 -0300

    feat: add initial folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Home from './Pages/Home/Home';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">Projeto Voluntários</h1>
-    </div>
+    <Home />
   );
 }
 
