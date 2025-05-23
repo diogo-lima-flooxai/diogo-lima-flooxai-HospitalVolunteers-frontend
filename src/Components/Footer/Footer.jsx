6f7084c880264469bb0f9919c3f3ea4a202daeee
diogo <diogo.lima.flooxai@gmail.com>
2025-05-23 08:36:56 -0300

    feat: add Component footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-gray-100 text-center py-6 mt-12 text-sm text-gray-600">
+    © 2025 Voluntários para Hospitais. Todos os direitos reservados.
+  </footer>
+);
+
+export default Footer;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,14 @@
-import React from "react"
+import React from "react";
 import Header from "../../Components/Header/HEader";
+import Footer from "../../Components/Footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
-
